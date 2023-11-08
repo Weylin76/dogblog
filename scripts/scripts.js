fbd0bd55f7424bd5373773088e3ebf7f088b997b
Weylin76 <Weylin76@msn.com>
2023-11-07 21:01:06 -0500

    fixed order of dogs oldest to youngest

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,6 @@
 function toggleMenu() {
-    var menu = document.getElementById('nav-menu');
-    if (menu.classList.contains('nav-menu-hidden')) {
-        menu.classList.remove('nav-menu-hidden');
-        menu.classList.add('nav-menu-visible');
-    } else {
-        menu.classList.remove('nav-menu-visible');
-        menu.classList.add('nav-menu-hidden');
-    }
+    var menu = document.getElementById("nav-menu");
+    menu.classList.toggle("nav-menu-hidden");
 }
+
+
